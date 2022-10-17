26132866d1b4e92f408f9b56400313509ad757b7
Gustavo Cardoso <cargugati@gmail.com>
2022-10-17 13:26:43 -0300

    TDD

---
+++ Calculadora/Calculadora.Tests/CalculadoraTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+
+namespace Calculadora.Tests
+{
+    [TestFixture]    
+    public class CalculadoraTests
+    {
+        [Test]
+        public void DeveAdicionarDoisNumeros()
+        {
+            //SUT
+            var sut = new CalculadoraSimples();
+
+            var resultado = sut.Adicionar(5, 5);
+
+            Assert.That(resultado, Is.EqualTo(10));
+        }
+
+        [Test]
+        public void DeveMultiplicarDoisNumeros()
+        {
+            //SUT
+            var sut = new CalculadoraSimples();
+
+            var resultado = sut.Multiplicar(5, 3);
+
+            Assert.That(resultado, Is.EqualTo(15));
+        }
+    }
+}
